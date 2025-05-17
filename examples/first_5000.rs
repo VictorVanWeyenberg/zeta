2e3bad4bfb084b088c3df92038673f8021f110b5
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-05-17 16:10:35 +0200

    Clean up example.

--- examples/first_5000.rs
+++ examples/first_5000.rs
@@ -5,14 +5,13 @@ use zeta::{zero_stream, SeekPattern, ZeroStream};
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let mut sink = ZeroSink::default();
-    zero_stream(&mut sink, SeekPattern::StartWithZeroNumberAmount(0, 5000)).await.expect("");
+    
+    let mut sink = ZeroSink;
+    let seek_pattern = SeekPattern::StartWithZeroNumberAmount(0, 5000);
+    zero_stream(&mut sink, seek_pattern).await.expect("");
 }
 
-#[derive(Default)]
-pub struct ZeroSink {
-    amt: usize,
-}
+pub struct ZeroSink;
 
 impl ZeroStream for ZeroSink {
     fn is_closed(&self) -> bool {
@@ -21,12 +20,5 @@ impl ZeroStream for ZeroSink {
 
     fn send(&mut self, zero_number: u64, zero: Float) {
         debug!("{zero_number}: {zero}");
-        self.track_amt();
-    }
-}
-
-impl ZeroSink {
-    fn track_amt(&mut self) {
-        self.amt += 1;
     }
 }
